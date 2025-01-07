c896974c0a22eca0bc7a486c894d55ef70d0bc45
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2025-01-07 12:20:30 -0300

    Refactor text-flow to streamline response handling
    - Removed random response messages to simplify the flow logic.
    - Consolidated the action handling within the textFlow function for improved readability and maintainability.
    - Maintained the core functionality of fetching and processing user input while enhancing code clarity.

--- src/flows/text-flow.ts
+++ src/flows/text-flow.ts
@@ -1,25 +1,17 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 
 const textFlow = () => {
-  return addKeyword(EVENTS.WELCOME)
-    .addAnswer(
-      [
-        "Dame un momento para pensar... ⌛",
-        "Dejame leer tu mensaje...",
-        "Hmmm, dame un momento...",
-      ][Math.floor(Math.random() * 3)]
-    )
-    .addAction(async (ctx, { ..._ }) => {
-      const n8nResponse = await fetch(new URL(process.env.API_ENTRY), {
-        method: "POST",
-        body: ctx.body,
-        headers: {
-          "x-phone-number": ctx.from,
-        },
-      }).then(async (r) => await r.text());
+  return addKeyword(EVENTS.WELCOME).addAction(async (ctx, { ..._ }) => {
+    const n8nResponse = await fetch(new URL(process.env.API_ENTRY), {
+      method: "POST",
+      body: ctx.body,
+      headers: {
+        "x-phone-number": ctx.from,
+      },
+    }).then(async (r) => await r.text());
 
-      await _.flowDynamic(n8nResponse);
-    });
+    await _.flowDynamic(n8nResponse);
+  });
 };
 
 export default textFlow;
