be2e079dd83e74479bfc7188424fbac8e352e1f8
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2025-01-10 16:40:45 -0300

    Fix typo in processing error message for improved user feedback

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,4 +1,4 @@
 export const ERROR_MESSAGES = {
   PROCESSING_ERROR:
-    "Ocurrió un error procesando tu mensaje. 😓\n Por favor volvé a intentar más tarde. ",
+    "Ocurrió un error procesando tu mensaje... 😓\nPor favor volvé a intentar más tarde. ",
 } as const;
